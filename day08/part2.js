3c708c262d96687bc2e1d78f76cce81a701860fc
Gabriel Ricard <4550801+gricard@users.noreply.github.com>
2017-12-08 22:03:51 -0500

    Day 8 solutions

---
+++ day08/part2.js
@@ -0,0 +1,59 @@
+fs = require('fs');
+
+// const input = fs.readFileSync('./testinput.txt'),
+const input = fs.readFileSync('./input.txt'),
+    lines = (new String(input)).split("\n");
+
+const commands = [],
+    registers = {};
+
+let maxValue = 0;
+
+lines.map((line) => {
+    const cmd = line.match(/(\S*) (inc|dec) (\S*) if (\S*) (\S*) (\S*)/);
+    commands.push(cmd);
+});
+
+for (let i = 0; i < commands.length; i++) {
+    const cmd = commands[i],
+        register = cmd[1],
+        action = cmd[2],
+        actionValue = parseInt(cmd[3]),
+        conditional1 = cmd[4],
+        comparison = cmd[5],
+        conditional2 = parseInt(cmd[6]);
+
+    let doAction = false;
+
+    if (registers[conditional1] === undefined) {
+        registers[conditional1] = 0;
+    }
+    
+    switch (comparison) {
+        case '==': doAction = registers[conditional1] == conditional2; break;
+        case '!=': doAction = registers[conditional1] != conditional2; break;
+        case '>': doAction = registers[conditional1] > conditional2; break;
+        case '<': doAction = registers[conditional1] < conditional2; break;
+        case '<=': doAction = registers[conditional1] <= conditional2;break;
+        case '>=': doAction = registers[conditional1] >= conditional2; break;
+    }
+
+    if (registers[register] === undefined) {
+        registers[register] = 0;
+    }
+
+    if (doAction) {
+        const prev = registers[register];
+        if (action == 'dec') {
+            registers[register] -= actionValue;
+        } else {
+            registers[register] += actionValue;
+        }
+
+        if (registers[register] > maxValue) {
+            maxValue = registers[register];
+        }
+    }
+}
+
+console.log('Max value during processing', maxValue);
